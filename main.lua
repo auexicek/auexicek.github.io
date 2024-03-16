bc73e2f12854c7c1ca576f9a71e7039e8d4680ce
auexicek <auex@duck.com>
2024-03-16 23:05:51 +0100

    added init and upd

--- main.lua
+++ main.lua
@@ -1,4 +1,18 @@
 --[[pod_format="raw",created="2024-03-16 21:20:17",modified="2024-03-16 21:22:25",revision=18]]
+function init()
+	pt="picotron"
+end
+
+function upd()
+	if key("space") then
+		if pt=="picotron" then
+			pt="world"
+		else
+			pt="picotron"
+		end
+	end
+end
+
 function drw()
-	print("hello picotron!",0,0,8)
+	print("hello "..pt.."!",0,0,18)
 end
